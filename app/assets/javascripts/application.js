71a6c948061b3693e8d9c384b476ad082d5b82bd
harshjxx <harshjxx@github.com>
2020-01-17 18:35:37 +0530

    nested form is added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,15 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+$(document).on('turbolinks:load', fucntion () {
+
+    $('form').on('click', '.add_fields', fucntion(event){
+      var regexp, time;
+      time= new Date().getTime();
+      regexp = new RegExp($(this).data('id'),'g');
+      $('.fields').append($(this).data('fields').replace(regexp, time));
+      return event.preventDefault();
+    });
+  });
